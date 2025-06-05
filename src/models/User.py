785828b93bf90b527cca547b5be6ba64a1fe0361
Melanie Dana Medigovich <melacem@gmail.com>
2025-06-05 15:59:30 +0000

    starwars models

--- src/models/User.py
+++ src/models/User.py
@@ -2,6 +2,7 @@ from database.db import db, Mapped, mapped_column, relationship
 from sqlalchemy import String, Boolean
 from typing import List
 
+
 class User(db.Model):
     __tablename__ = 'user'
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -11,12 +12,13 @@ class User(db.Model):
     email: Mapped[str] = mapped_column(String(120), unique=True)
     password: Mapped[str] = mapped_column(String(80))
     is_active: Mapped[bool] = mapped_column(Boolean(), default=True)
-    
+
     favorites = relationship("Favorite", backref="user")
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            "username": self.username
+            "username": self.username,
+            "favorites": [fav.serialize() for fav in self.favorites]
         }
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,6 +2,6 @@ from .Character import Character
 from .Planet import Planet
 from .Vehicle import Vehicle
 from .Favorite import Favorite
-from .User import User  # Actualizado desde models.py
+from .User import User
 
 __all__ = ['User', 'Character', 'Planet', 'Vehicle', 'Favorite']
